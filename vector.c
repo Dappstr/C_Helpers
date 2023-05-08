450da551309b7fd00719bdc99a25f31c0320745f
Lane Beals <lanebeals02@gmail.com>
2023-05-07 19:13:19 -0700

    Modify vector.c

--- vector.c
+++ vector.c
@@ -11,11 +11,18 @@
 
 void* vector_begin(Vector* vec) {
     assert(vec->m_size >= 1);
-    return &vec->m_buffer[vec->m_size-1]; 
+    return &vec->m_buffer; 
 }
 
 void* vector_end(Vector* vec) {
-
+    assert(vec->m_size >= 1);
+    return (char*)vec->m_buffer + vec->m_size * vec->m_elem_size; // Get the last area of memory
+                                                             // We first cast to a char pointer to allow for pointer arithmetic
+                                                             // Since C cannot do pointer arithmetic on type of void*
+                                                             // We get the product of m_size and m_elem_size because
+                                                             // the amount of memory allocated will be the amount times the size of the type
+                                                             // so a buffer of 3 ints will be 3 * 4 since an int is 4 bytes, and then add on m_buffer
+                                                             // to get the memory location that is 1 byte past.
 }
 
 // elem_size can be something like sizeof(int)
