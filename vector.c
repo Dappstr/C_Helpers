034e9af251935668238be2d29784c2b08b0f4cdb
Lane Beals <lanebeals02@gmail.com>
2023-05-07 18:48:22 -0700

    Included some code in vector files

--- vector.c
+++ vector.c
@@ -2,6 +2,26 @@
 #define VECTOR_H
 
 #endif
+#include "vector.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "assert.h"
 
 //...TODO
 
+void* vector_begin(Vector* vec) {
+    assert(vec->m_size >= 1);
+    return &vec->m_buffer[vec->m_size-1]; 
+}
+
+void* vector_end(Vector* vec) {
+
+}
+
+// elem_size can be something like sizeof(int)
+void init_vector(Vector* vec, int elem_size) {
+    vec->m_buffer = NULL;
+    vec->m_size = 0;
+    vec->m_cap = 0;
+    vec->m_elem_size = elem_size;
+}
--- vector.h
+++ vector.h
@@ -6,4 +6,12 @@ typedef struct
     void* m_buffer; // Buffer containing data
     int m_size; // Current size of buffer/current amount of elements taken up
     int m_cap; // Maximum capacity of vector
+    int m_elem_size; // Size per element. I.e, if there are ints, it will be of size 4
 } Vector;
+
+void* vector_begin(Vector* vec);
+void* vector_end(Vector* vec);
+void vector_pop(Vector* vec);
+void vector_push(Vector* vec);
+void vector_push_back(int val);
+void vector_init(Vector* vec);
