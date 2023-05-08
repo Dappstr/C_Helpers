14c922a37b46e407cbb48fe768e470e8c53d3851
Lane Beals <lanebeals02@gmail.com>
2023-05-07 18:27:47 -0700

    Changed vector to properly typedef'ed

--- vector.h
+++ vector.h
@@ -1,9 +1,9 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-struct Vector
+typedef struct
 {
     void* m_buffer; // Buffer containing data
     int m_size; // Current size of buffer/current amount of elements taken up
     int m_cap; // Maximum capacity of vector
-};
+} Vector;
